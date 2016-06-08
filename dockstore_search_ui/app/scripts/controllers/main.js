2ba05284313c594de2468aaf14188bde42294e0e
spacime <spacimeulim@gmail.com>
2016-06-08 10:18:13 -0700

    initial ui dict

---
+++ dockstore_search_ui/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dockstoreSearchUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dockstoreSearchUiApp
+ */
+angular.module('dockstoreSearchUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
