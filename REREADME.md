6086497f282ed6e3e4c0c5fd8a38f6bca5e62b62
spacime <spacimeulim@gmail.com>
2016-06-08 11:34:13 -0700

    add parameter to RESTful server

---
+++ REREADME.md
@@ -0,0 +1,2 @@
+# Introduction
+This repository contains the code to realize search function cross various sites for [dockstore](https://github.com/ga4gh/dockstore).
--- dockstore_search_server/sever.py
+++ dockstore_search_server/sever.py
@@ -1,14 +1,18 @@
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-@app.route("/ulim/<name>")
+@app.route("/search_keywords=<name>")
 def limhello(name):
-    return "%s, hello" % name
+    conn_string = "host='localhost' dbname='dockstore' user='ulim' password='3233173'"
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+    cur.execute("SELECT author, name FROM gmod_tools;")
+    authors = cur.fetchall()
+    cur.close()
+    conn.close()
+    return str(authors)
 
 if __name__ == "__main__":
     app.run()
