205f09759763d943fa75bb7ed61502c09a8842dd
spacime <spacimeulim@gmail.com>
2016-06-09 10:38:48 -0700

    able to show author and name

--- dockstore_search_ui/app/scripts/controllers/main.js
+++ dockstore_search_ui/app/scripts/controllers/main.js
@@ -7,11 +7,55 @@
  * # MainCtrl
  * Controller of the dockstoreSearchUiApp
  */
+
 angular.module('dockstoreSearchUiApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope, $http) {
+  $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+  $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+
+//   $scope.students = [
+//    {
+//       "Name" : "Mahesh Parashar",
+//       "RollNo" : 101,
+//       "Percentage" : "80%"
+//    },
+//
+//    {
+//       "Name" : "Dinkar Kad",
+//       "RollNo" : 201,
+//       "Percentage" : "70%"
+//    },
+//
+//    {
+//       "Name" : "Robert",
+//       "RollNo" : 191,
+//       "Percentage" : "75%"
+//    },
+//
+//    {
+//       "Name" : "Julian Joe",
+//       "RollNo" : 111,
+//       "Percentage" : "77%"
+//    }
+// ];
+
+
+              var my_url = "http://127.0.0.1:5000/search_keywords=bri";
+
+              $http({
+                  method : "GET",
+                  url : "http://127.0.0.1:5000/search_keywords=bri"
+              }).then(function mySucces(response) {
+                  // $scope.myWelcome = JSON.parse(response.data);
+                  console.log(response);
+                  $scope.tools = response.data;
+              }, function myError(response) {
+                  $scope.tools = response.statusText;
+              });
+
+              $scope.info = "hello"
+
+});
