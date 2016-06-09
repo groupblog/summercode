9f645fd87b99828233c4f140f139db1a87f9a7bf
zhaot777 <zhaot777@gmail.com>
2016-06-09 11:30:43 -0700

    Update search_server.py

--- dockstore_search_server/search_server.py
+++ dockstore_search_server/search_server.py
@@ -4,14 +4,21 @@ app = Flask(__name__)
 
 @app.route('/search/<key>', methods=['GET'])
 def search(key):
-	patten = '%'+key+'%'
+	patten = '%' + key + '%'
 	result_list = []
 	db_info = "dbname='mydb' user='postgres' password='12345' host='localhost'"
 	conn = psycopg2.connect(db_info)
 	cur = conn.cursor()
-	cur.execute("SELECT * FROM tools WHERE name LIKE %s OR author LIKE %s;",(patten, patten))
+	cur.execute("SELECT name, author, registryid FROM tools WHERE name LIKE %s OR author LIKE %s;",(patten, patten))
 	result_list = cur.fetchall()
-	return json.dumps(result_list)
+	lists = []
+	for item in result_list:
+		dirc = {} 
+		dirc['name'] = item[0]
+		dirc['author'] = item[1]
+		dirc['docker pull'] = item[2]
+		lists.append(dirc)
+	return json.dumps(lists)
 
 if __name__ == '__main__':
 	app.run()
