10534ea3eca75451c72950955fabc35b185d6b00
spacime <spacimeulim@gmail.com>
2016-08-02 20:54:52 -0700

    add version

--- dockstore_jar/test.sh
+++ dockstore_jar/test.sh
@@ -1,6 +1,56 @@
-ret=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
-if [ $ret -eq 0 ]; then
-    echo "we require python version <3"
+# Check java
+if type -p java > /dev/null; then
+  _java=java
 else
-    echo "python version is <3"
+  echo "Java was not installed or is not on your PATH."
+  exit -100;
+fi
+
+if [[ "$_java" ]]; then
+  version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+  if [[ "$version" < "1.8" ]]; then
+    echo "Dockstore requires Java version 1.8 and above."
+    exit -100;
+  else
+    echo "The java version $version"
+  fi
+fi
+
+# Check Existence Python
+if type -p python > /dev/null; then
+  _python=python
+  echo "find python"
+else
+  echo "python was not installed or is not on your PATH."
+  exit -100;
+fi
+# Check the python version
+if [[ "$_python" ]]; then
+  version=$("$_python" --version 2>&1 | awk '{print $2}')
+  if [[ "$version" < "2.7" ]]; then
+    echo "Dockstore requires Java version 1.8 and above."
+    # exit -100;
+  else
+    echo "Python's version $version"
+  fi
+fi
+
+#Check Docker
+if type -p docker > /dev/null; then
+  _docker=docker
+  echo "find docker"
+else
+  echo "docker was not installed or is not on your PATH."
+  exit -100;
+fi
+
+if [[ "$_docker" ]]; then
+  docker_version=$("$_docker" --version 2>&1 | awk '{print $3}')
+  docker_version=${docker_version::-1}
+  if [[ "$version" < "1.0" ]]; then
+    echo "Dockstore requires docker version 1.1 and above."
+    exit -100;
+  else
+    echo "The docker version is $docker_version"
+  fi
 fi
